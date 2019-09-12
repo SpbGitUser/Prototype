254705150f740b7a50c21b6af5f3bf00068f8388
Area777 <kagor32@mail.ru>
2019-09-12 12:53:36 +0300

    Init

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassLibrary1
+{
+    [ComVisible(true)]
+    [Guid("82B579BE-64D9-4F97-BBFC-FFB8597C364D")]
+    public class Class1
+    {
+        private string mes;
+        public Class1()
+        {
+            mes = "Hi!";
+        }
+
+        public void Say()
+        {
+            MessageBox.Show(mes);
+        }
+    }
+
+    [Guid("4ACF7988-55F2-4BE1-9FD5-4CCB0BE83335")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIDispatch)]
+    [ComVisible(true)]
+    public interface IClass1
+    {
+        void Say();
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+
+        private enum En
+        {
+            First = 15,
+            Second,
+            Third = 54
+        }
+
+        static void Main(string[] args)
+        {
+            EmptyString();
+            //Test1();
+            //test2();
+
+
+            Console.ReadLine();
+            
+        }
+
+        private static void test2()
+        {
+            var array = new int[] { 1, 2 };
+            try
+            {
+                Console.Write(array[5]);
+            }
+            catch (ApplicationException e)
+            {
+                Console.Write(1);
+            }
+            catch (SystemException e)
+            {
+                Console.Write(array[5]);
+            }
+            catch (Exception e)
+            {
+                Console.Write(3);
+            }
+        }
+
+        private static void Test1()
+        {
+            Console.WriteLine((int)En.Second);
+        }
+
+        private static void EmptyString()
+        {
+            string a = null;
+            var b = a.Replace("\r\n", string.Empty);
+            Console.WriteLine(b);
+        }
+    }
+}
