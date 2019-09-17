eef8216003c4b37a8423d6e5464503470c7e68a3
Area777 <kagor32@mail.ru>
2019-09-17 10:10:23 +0300

    Наследование

--- ConsoleApp/Classes.cs
+++ ConsoleApp/Classes.cs
@@ -1,19 +1,129 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
     public class Classes
     {
+
+        #region Наследование (inheritance)
+
+        internal /*sealed*/ class Man
+        {
+            public Man()
+            {
+                //Console.WriteLine("Man's Basic");
+            }
+
+            public Man(string name) : base()
+            {
+                _name = name;
+                //Console.WriteLine("Man's Band");
+            }
+
+            private string _name;
+            public string Name
+            {
+                get { return _name; }
+                set { _name = value; }
+            }
+
+            public void Say()
+            {
+                //Интерполя́ция, интерполи́рование — в вычислительной математике способ нахождения 
+                //промежуточных значений величины по имеющемуся дискретному набору известных значений.
+
+                Console.WriteLine($"Hi! I'm {_name}.");
+            }
+        }
+
+        internal class Boy : Man
+        {
+            public Boy(string boyName) : base(boyName)
+            {   
+                //Console.WriteLine("Boy's Band");
+            }
+
+            public void SayMore()
+            {
+                Console.WriteLine($"Boy's name is {Name}!");
+            }
+        }
+
+        #endregion Наследование (inheritance)
+
+
+
+        #region Indextors
+
+        internal class Person
+        {
+            public string Name { get; set; }
+        }
+
+        internal class People
+        {
+            private Person[] _data;
+
+            public People()
+            {
+                _data = new Person[5];
+            }
+
+            public Person this[int index]
+            {
+                get
+                {
+                    return _data[index];
+                }
+                set
+                {
+                    _data[index] = value;
+                }
+            }
+            public string this[string index]
+            {
+                get
+                {
+                    var res = string.Empty;
+                    switch (index)
+                    {
+                        case "1":
+                            res = _data[0].Name;
+                            break;
+                        case "2":
+                            res = _data[1].Name;
+                            break;
+                        case "3":
+                            res = _data[2].Name;
+                            break;
+                        case "4":
+                            res = _data[3].Name;
+                            break;
+                        case "5":
+                            res = _data[4].Name;
+                            break;
+                    }
+                    return res;
+                }
+            }
+        }
+
+        #endregion Indextors
+
+
+        #region Статической свойство
+        
         internal class WithStaticMemeber
         {
             public static int Age { get; set; }
 
             public string Name { get; set; }
         }
+
+        #endregion Статической свойство
+
+
+        #region Перегрузка оператора
 
         internal class Counter
         {
@@ -24,6 +134,8 @@ namespace ConsoleApp
 
             public int Value { get; set; }
         }
+
+        #endregion Перегрузка оператора
 
     }
 }
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,18 +10,84 @@ namespace ConsoleApp
 
         static void Main(string[] args)
         {
-            //EmptyString();
-            //StringNullTest1();
-            //ArreyTest1();
-            //StaticMember();
-            OperatorOverridingTest();
+            try
+            {
+                //EmptyString();
+                //StringNullTest1();
+                //ArreyTest1();
+                //StaticMember();
+                //OperatorOverridingTest();
+                //Indexator();
+                //Inheritance();
+                TypesConvertion();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                //throw;
+            }
 
-            Console.ReadLine();
-            
+
+            Console.ReadKey();
         }
 
         //----------------_T_E_S_T_I_N_G_-------------------
 
+
+        private static void TypesConvertion()
+        {
+            var boy = new Boy("BoyName");
+            //var men = boy as Man;
+            //men.Name = "ManName";
+            //boy.SayMore();
+            Man man2 = boy;
+            man2.Say();
+            Console.WriteLine("----------------------------");
+            var man1 = new Man("ManName");
+            if (man1 is Boy)
+            {
+                Boy boy2 = (Boy)man1;
+                boy2?.SayMore();
+            }
+            else
+            {
+                Console.WriteLine("Man is not a boy :)");
+            }
+        }
+
+
+        private static void Inheritance()
+        {
+            //var men = new Man("Ilya");
+            var boy = new Boy("Nikola");
+            //var boy2 = men as Boy;
+            //var men2 = boy as Man;
+            //men2?.Say();
+            //boy2?.Say();
+            //boy?.SayMore();
+        }
+
+        private static void Indexator()
+        {
+            var people = new People
+            {
+                [0] = new Person() {Name = "Anton"},
+                [1] = new Person() {Name = "Max"},
+                [2] = new Person() {Name = "Ivan"},
+                [3] = new Person() {Name = "Dexter"},
+                [4] = new Person() {Name = "Slava"}
+            };
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(people[i].Name);
+            }
+
+            Console.WriteLine("----------------------------");
+
+            Console.WriteLine(people["456"] ?? "this is NULL");
+            Console.WriteLine(people["1"]);
+        }
 
         private static void OperatorOverridingTest()
         {
