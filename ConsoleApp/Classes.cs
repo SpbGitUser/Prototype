507ff6b3dec2ce6b77942c15a6716ce741c9fc8a
Area777 <kagor32@mail.ru>
2019-09-17 15:32:56 +0300

    Наследование + исаключения

--- ConsoleApp/Classes.cs
+++ ConsoleApp/Classes.cs
@@ -5,9 +5,122 @@ namespace ConsoleApp
     public class Classes
     {
 
-        #region Наследование (inheritance)
+        #region Exceptions / Исключения
 
-        internal /*sealed*/ class Man
+        internal class MyException : Exception
+        {
+            public MyException(string mes) : base(mes)
+            {
+            }
+        }
+
+        internal class ExceptionTestClass
+        {
+            public static void Method1()
+            {
+                try
+                {
+                    Method2();
+                }
+                catch (IndexOutOfRangeException ex)
+                {
+                    Console.WriteLine($"Catch в Method1 : {ex.Message}");
+                }
+                finally
+                {
+                    Console.WriteLine("Блок finally в Method1");
+                }
+                Console.WriteLine("Конец метода Method1");
+            }
+            static void Method2()
+            {
+                try
+                {
+                    int x = 8;
+                    int y = x / 0;
+                }
+                finally
+                {
+                    Console.WriteLine("Блок finally в Method2");
+                }
+                Console.WriteLine("Конец метода Method2");
+            }
+        }
+
+        #endregion
+
+
+        #region Generics / Обобщения
+
+        internal class Account<T>
+        {
+            public static T Value;
+
+            public T Id { get; private set; } // номер счета
+            public int Sum { get; set; }
+            public Account(T id)
+            {
+                Id = id;
+            }
+        }
+
+        internal class MyAccount : Account<string>
+        {
+            public MyAccount(string id) : base(id)
+            {
+            }
+        }
+
+        internal class Transaction<T> where T : Account<int>
+        {
+            public T FromAcc { get; private set; }
+
+            public T ToAcc { get; private set; }
+
+            public Transaction(T from, T to)
+            {
+                FromAcc = from;
+                ToAcc = to;
+            }
+        }
+
+        #endregion Generics / Обобщения
+
+
+        #region Наследование (inheritance) / преобразование типов
+
+        internal class Mikron
+        {
+            public Mikron()
+            {
+                Console.WriteLine("Mikron creaqted!");
+            }
+        }
+
+        /// <summary>
+        /// Человек
+        /// </summary>
+        internal abstract class Human : Mikron
+        {
+            public string SurName { get; set; }
+
+            public abstract ushort Age { get; set; }
+
+            public void SayType()
+            {
+                Console.WriteLine(this.GetType().ToString());
+            }
+        }
+
+        internal abstract class Homosapien : Human
+        {
+            
+        }
+
+        /// <summary>
+        /// Мужчина
+        /// </summary>
+        internal /*sealed*/ class Man : Human
         {
             public Man()
             {
@@ -21,25 +134,29 @@ namespace ConsoleApp
             }
 
             private string _name;
-            public string Name
+            public virtual string Name
             {
                 get { return _name; }
                 set { _name = value; }
             }
 
-            public void Say()
+            public override ushort Age { get; set; }
+
+            public virtual void Say()
             {
                 //Интерполя́ция, интерполи́рование — в вычислительной математике способ нахождения 
                 //промежуточных значений величины по имеющемуся дискретному набору известных значений.
-
-                Console.WriteLine($"Hi! I'm {_name}.");
+                Console.WriteLine($"Hi! I'm MAN {_name}.");
             }
         }
 
+        /// <summary>
+        /// Мальчик
+        /// </summary>
         internal class Boy : Man
         {
             public Boy(string boyName) : base(boyName)
-            {   
+            {
                 //Console.WriteLine("Boy's Band");
             }
 
@@ -47,13 +164,94 @@ namespace ConsoleApp
             {
                 Console.WriteLine($"Boy's name is {Name}!");
             }
+
+            public override void Say()
+            {
+                Console.WriteLine($"Boy {Name}");
+            }
         }
 
-        #endregion Наследование (inheritance)
+        /// <summary>
+        /// Парень
+        /// </summary>
+        internal class BigBoy : Boy
+        {
+            public BigBoy(string boyName) : base(boyName)
+            {
+            }
+
+            private string _name;
+            public override string Name
+            {
+                get
+                {
+                    return $"{_name} ;)";
+                }
+                set
+                {
+                    _name = value;
+                }
+            }
+
+            public new void SayMore()
+            {
+                Console.WriteLine($"BIG boy's name is {Name}!");
+            }
+
+            public static explicit operator BigBoy(string name)
+            {
+                var bb = new BigBoy(name);
+                return bb;
+            }
+
+            public override void Say()
+            {
+                Console.WriteLine($"BigBoy {Name}");
+            }
+        }
+
+        /// <summary>
+        /// Женщина
+        /// </summary>
+        internal class Women : Human
+        {
+            public Women(string womenName) //: base(boyName)
+            {
+                //Console.WriteLine("Women's Band");
+                Name = womenName;
+            }
+            
+            public string Name { get; set; }
+
+            public override ushort Age { get; set; }
+
+            public void SayMore()
+            {
+                Console.WriteLine($"Women's name is {Name}!");
+            }
+
+            public static implicit operator Boy(Women param)
+            {
+                var boy = new Boy(param.Name);
+                return boy;
+            }
+
+            public static implicit operator Women(string name)
+            {
+                var res = new Women(name);
+                return res;
+            }
+
+            public void Say()
+            {
+                Console.WriteLine($"I'm WOMEN - {Name}");
+            }
+        }
+
+        #endregion Наследование (inheritance) / преобразование типов
 
 
-
-        #region Indextors
+        #region Indextors / Индексаторы
 
         internal class Person
         {
@@ -108,11 +306,11 @@ namespace ConsoleApp
             }
         }
 
-        #endregion Indextors
+        #endregion Indextors / Индексаторы
 
 
         #region Статической свойство
-        
+
         internal class WithStaticMemeber
         {
             public static int Age { get; set; }
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -19,7 +19,19 @@ namespace ConsoleApp
                 //OperatorOverridingTest();
                 //Indexator();
                 //Inheritance();
-                TypesConvertion();
+                //TypesConvertion();
+                //TypesConvertion2();
+                //TypesConvertion3();
+                //TypesConvertion4();
+                //TypesConvertion5();
+                //TypesConvertion6();
+                //TypesConvertion7();
+                //EqualsTest();
+                //EqualsTest1();
+                //GenricsTest();
+                //TryCatchTest();
+                //TryCatchTest1();
+                TryCatchTest2();
             }
             catch (Exception e)
             {
@@ -32,6 +44,185 @@ namespace ConsoleApp
         }
 
         //----------------_T_E_S_T_I_N_G_-------------------
+        //Console.WriteLine("------------------");
+
+        private static void TryCatchTest2()
+        {
+            try
+            {
+                ExceptionTestClass.Method1();
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"Catch в Main : {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Блок finally в Main");
+            }
+            Console.WriteLine("Конец метода Main");
+            Console.Read();
+        }
+
+        private static void TryCatchTest1()
+        {
+            try
+            {
+                int x;
+                if (!int.TryParse(Console.ReadLine(), out x))
+                {
+                    throw new MyException("This is not INT");
+                }
+            }
+            catch (MyException ex)
+            {
+                Console.WriteLine($"Hey!!! {ex.Message}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private static void TryCatchTest()
+        {
+            int x = 5;
+
+            try
+            {
+                //x *= x;
+                //Console.WriteLine(x);
+                x = Int32.Parse(Console.ReadLine() ?? "3"); ;
+                int y = x / 0;
+                Console.WriteLine($"Результат: {x}");
+            }
+            //catch (DivideByZeroException e) when (x > 5)
+            //{
+            //    Console.WriteLine("When exception");
+            //}
+            //catch (DivideByZeroException e) 
+            //{
+            //    Console.WriteLine("Simple ex");
+            //}
+            finally
+            {
+                Console.WriteLine("Блок finally");
+            }
+        }
+        
+        private static void GenricsTest()
+        {
+            var acc1 = new Account<int>(100)
+            {
+                Sum = 100
+            };
+            //var acc2 = new Account<string>(200)
+            //{
+            //    Sum = 500
+            //};
+            
+
+            var ma = new MyAccount("7896-1425-1231-4565");
+            Console.WriteLine(ma.Id.GetType());
+            Account<string> acc = new MyAccount("sdfs-sdfs-sfdd-sdfsd");
+        }
+
+        private static void EqualsTest1()
+        {
+            Person person1 = new Person {Name = "Tom"};
+            Person person2 = new Person {Name = "Bob"};
+            Person person3 = new Person {Name = "Tom"};
+            bool p1Ep2 = person1.Equals(person2); // false
+            Console.WriteLine(p1Ep2);
+            bool p1Ep3 = person1.Equals(person3); // true
+            Console.WriteLine(p1Ep3);
+        }
+
+        private static void EqualsTest()
+        {
+            var str = "11";
+            var str1 = "11";
+            var c = 11;
+            var d = 11;
+            if (str.Equals(str1))
+            {
+                Console.WriteLine("Strings equals");
+            }
+            if (d.Equals(c))
+            {
+                Console.WriteLine("Integer equals");
+            }
+        }
+
+        private static void TypesConvertion7()
+        {
+            Human m = new Man("Jack");
+            Human m1 = new Man("Jack");
+            Human w = new Women("Ann");
+            Human bb = new BigBoy("Billy");
+            Human bb1 = new BigBoy("Billy");
+            Human bbb = bb;
+            m.SayType();
+            w.SayType();
+            bb.SayType();
+            Console.WriteLine("------------------");
+            object obj = m;
+            Console.WriteLine(obj.GetType());
+            
+        }
+
+        private static void TypesConvertion6()
+        {
+            Boy boy = new BigBoy("Sam");
+            boy.Say();
+            boy.SayMore();
+        }
+
+        private static void TypesConvertion5()
+        {
+            Man boy = new BigBoy("Sam");
+            if (boy is Man)
+            {
+                Console.WriteLine("is man");
+            }
+            if (boy is BigBoy)
+            {
+                Console.WriteLine("is Big boy");
+            }
+            if (boy is object)
+            {
+                Console.WriteLine("is object");
+            }
+        }
+
+        private static void TypesConvertion4()
+        {
+            Man boy = new BigBoy("Sam");
+            Boy bb = (Boy) boy;
+            bb.Say();
+        }
+
+        private static void TypesConvertion3()
+        {
+            var str = "Jane";
+            Women wma = str;
+            wma.SayMore();
+            wma.Say();
+        }
+
+        private static void TypesConvertion2()
+        {
+            var boy2 = new Women("Tom");
+            Boy boy = (Boy) boy2;
+            Boy boy1 = boy2;
+            boy.SayMore();
+            boy1.SayMore();
+            Console.WriteLine("------------------");
+            var str = "Jane";
+            Women wma = str;
+            wma.SayMore();
+            wma.Say();
+        }
 
 
         private static void TypesConvertion()
@@ -42,7 +233,7 @@ namespace ConsoleApp
             //boy.SayMore();
             Man man2 = boy;
             man2.Say();
-            Console.WriteLine("----------------------------");
+            Console.WriteLine("------------------");
             var man1 = new Man("ManName");
             if (man1 is Boy)
             {
@@ -83,7 +274,7 @@ namespace ConsoleApp
                 Console.WriteLine(people[i].Name);
             }
 
-            Console.WriteLine("----------------------------");
+            Console.WriteLine("------------------");
 
             Console.WriteLine(people["456"] ?? "this is NULL");
             Console.WriteLine(people["1"]);
