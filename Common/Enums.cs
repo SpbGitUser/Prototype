d11085ef33a2fa915dd54edd5a63261242c4d574
Area777 <kagor32@mail.ru>
2020-02-11 11:51:48 +0300

    Небольшая реструктуризация

---
+++ Common/Enums.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Common
+{
+    public static class Enums
+    {
+        /// <summary>
+        /// Режим запуска
+        /// </summary>
+        public enum StarterMode
+        {
+            Unknown = 0,
+            StaticTests = 1,
+            QuestionTests = 2,
+            Threading = 3,
+            Ado = 4
+        }
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using Common;
 using static System.Console;
 using static ConsoleApp.Common.Utils;
 using static ConsoleApp.Threading.ThreadingTests;
@@ -6,22 +8,66 @@ using static ConsoleApp.StaticTests.StaticTests;
 
 namespace ConsoleApp
 {
+
     public class Program
     {
-        //--ЗАПУСК-------------------------------------------
-        static void Main(string[] args)
+        /// <summary>
+        /// --ЗАПУСК-------------------------------------------
+        /// </summary>
+        public static void Main(string[] args)
         {
             try
             {
                 W("-------------------- START --------------------" + Environment.NewLine);
-                
-                //тесты в виде статических методов
-                RunStaticTests();
-                //тестовые ситуации
-                //QuestionTests();
-                //потоки
-                //RunThreading();
-                
+                Enums.StarterMode mode;
+                do
+                {
+                    W("SELECT MODE:"+ Environment.NewLine);
+                    W("> 1 - StaticTests");
+                    W("> 2 - QuestionTests");
+                    W("> 3 - Threading");
+                    W("> 4 - ADO.Net");
+                    var key = ReadKey();
+                    var keyChar = key.KeyChar.ToString();
+                    try
+                    {
+                        var modeId = int.Parse(keyChar);
+                        if (!Enum.IsDefined(typeof(Enums.StarterMode), modeId))
+                        {
+                            throw new Exception(string.Empty);
+                        }
+                        mode = (Enums.StarterMode)Enum.ToObject(typeof(Enums.StarterMode), modeId);
+                        if (mode != Enums.StarterMode.Unknown)
+                        {
+                            W(Environment.NewLine);
+                            W($"------SELECTED MODE IS {mode} --------------------" + Environment.NewLine);
+                        }
+                    }
+                    catch
+                    {
+                        mode = Enums.StarterMode.Unknown;
+                    }
+                }
+                while (mode == Enums.StarterMode.Unknown);
+
+                switch (mode)
+                {
+                    case Enums.StarterMode.StaticTests:
+                        //тесты в виде статических методов
+                        RunStaticTests();
+                        break;
+                    case Enums.StarterMode.QuestionTests:
+                        //тестовые ситуации
+                        QuestionTests();
+                        break;
+                    case Enums.StarterMode.Threading:
+                        //потоки
+                        RunThreading();
+                        break;
+                    case Enums.StarterMode.Ado:
+                        W(Environment.NewLine);
+                        break;
+                }
                 ReadKey();
                 W(Environment.NewLine + "------------------- THE_END -------------------");
             }
